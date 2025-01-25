1001ba4cd2e6e78384fd5ad3b99976e1682beaf6
Matthew Gray <phlip45@gmail.com>
2025-01-24 21:29:03 -0800

    Fixed input stuff

--- cgj25/Enemy.gd
+++ cgj25/Enemy.gd
@@ -3,9 +3,10 @@ extends CharacterBody2D
 @export var path_points: Array[Marker2D]
 @export var nav_region:NavigationRegion2D
 @onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
+@onready var marker_parent = $Markers
 var nav_region_id:RID
 var current_target:int = 0
-@onready var marker_parent = $Markers
+var speed_max:float = 500
 
 func _ready() -> void:
 	nav_region_id = nav_region.get_region_rid()
@@ -20,16 +21,19 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	#print(nav_region_id)
 	var next_pos = navigation_agent_2d.get_next_path_position()
-	velocity = Vector2(global_position.direction_to(next_pos).x*100,global_position.direction_to(next_pos).y*100)
+	velocity = Vector2(global_position.direction_to(next_pos).x*speed_max,global_position.direction_to(next_pos).y*speed_max)
 	move_and_slide()
 	if global_position.distance_to(next_pos) < 10:
 		current_target = (current_target + 1) % path_points.size()
 		navigation_agent_2d.set_target_position(path_points[current_target].position)
 		pass
 
-
-func _input(event):
-	if event.is_action_pressed("add_point"):
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("speed_up"):
+		speed_max = 500
+	else:
+		speed_max = 100
+	if Input.is_action_just_pressed("add_point"):
 		# Generate a random point within a certain range
 		var random_point = Vector2(
 			randi() % 400 - 200,  # Adjust the range as needed
@@ -47,7 +51,3 @@ func _input(event):
 		# If the path_points array was empty, set the new target
 		if path_points.size() == 1:
 			navigation_agent_2d.set_target_position(random_point)
-	if event.is_action_pressed("speed_up"):
-		velocity.x += 10
-		velocity.y += 10
-		
